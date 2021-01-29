b9517bd2e6f2c19f69868b31a311db409aca4c91
mmcginley81 <matthewjamesmcginley@gmail.com>
2021-01-28 20:18:09 -0800

    starting function save

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,22 @@ let PerfectDay = []
 let searchButtonEl = document.querySelector("button");
 
 //When I enter my city into the search bar I will fetch the current weather from OpenWeatherAPI
+
+function searchWeather(city){
+    let currentWeather = document.querySelector('#current-weather').empty()
+    
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=f38f6a7de25e9c5bfba8b768dc8d3f45&units=imperial`)
+    .then(response => response.json())
+    .then(response => {
+        //Check for correct city spelling and if correct run function
+        if (response.message === "city not found"){
+            alert("City not found, please check your spelling and try again.")
+        }else{
+            let weatherCard = document.querySelector("#weather-container").addClass('card');
+            const cityName = document.querySelector('<h3>').addClass('card-title').text(`${response.name}`)
+            const date = $('<h3>').addClass('card-title').text(new Date().toLocaleDateString())
+            const icon = $('<img>').attr('src', `http://openweathermap.org/img/w/${response.weather[0].icon}.png`);
+            const temp = $('<p>').addClass('card-text').text(`temperature: ${response.main.temp} °F`)
+            const humidity = $('<p>').addClass('card-text').text(`humidity: ${response.main.humidity} %`)
+            const wind = $('<p>').addClass('card-text').text(`wind-speed: ${response.wind.speed} mph`)
+            weatherCard.append(cityName, date, icon, temp, humidity, wind)
