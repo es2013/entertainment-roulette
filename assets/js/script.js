de7c4962b16746f7dbc748340ae4b5c0fbb9b918
Erika <erikasalcedo@Erikas-MacBook-Pro.local>
2021-01-28 20:21:50 -0800

    updated getweather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,37 @@ let PerfectDay = []
 let searchButtonEl = document.querySelector("button");
 
 //When I enter my city into the search bar I will fetch the current weather from OpenWeatherAPI
+
+
+
+
+//call the displayCity function when button is clicked
+searchButtonEl.addEventListener("click", );
+
+
+let appID = "d1dfd9b71c61f4f9b6151a02ee936efa"
+
+function getWeather() {
+    // Use `.value` to capture the value of the input and store it in the variable
+    var searchCity = document.querySelector('#city-input').value;
+    fetch(
+        `http://api.openweathermap.org/data/2.5/weather?&appid=${appID}&q=${searchCity}` + '&units=imperial'
+
+    )
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            //handle error for invalid city inputs
+            if (data.cod == 404) {
+                alert("Hmmm, sorry, I couldn't identify your city. Try a different city.");
+                return;
+             }
+            let currentWeather = document.querySelector("#current-city");
+            document.querySelector("#current-city").textContent = data.name;
+
+            //update content for other html fields using ID and textContent to replace values
+            document.querySelector("#temperature").textContent = " " + data.main.temp + " º F";
+        })
+    };
